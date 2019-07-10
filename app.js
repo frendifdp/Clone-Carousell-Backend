8db1206e19c3e6920018e902ab6afb857e25bdb6
frendifdp <frendifdp@gmail.com>
2019-07-10 09:47:51 +0700

    cleaning unused code

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app		      = express();
 
 // routes
 const productsRoute       = require('./src/routes/products');
-//const usersRoute 	    = require('./src/routes/products');
+const usersRoute 	    = require('./src/routes/products');
 
 app.listen(port);
 console.log('Server Runing '+port);
@@ -14,7 +14,5 @@ console.log('Server Runing '+port);
 app.use( bodyParser.urlencoded({ extended:false }) );
 app.use(bodyParser.json());
 
-// app.use('/products', productsRoute);
 app.use('/users', usersRoute);
-
 app.use('/products', productsRoute)
