82592b4c886fd6bc0ca1b0446aa7c559e61f8df3
DanyAdhi <45444857+DanyAdhi@users.noreply.github.com>
2019-07-10 11:02:05 +0700

    coba heroku

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 require('dotenv/config');
 const express 	  = require('express');
 const bodyParser  = require('body-parser');
-const port 		    = process.env.PORT || 5000;
-const app		      = express();
+const port 		  = process.env.PORT || 5000;
+const app		  = express();
 
-const usersRoute 	    = require('./src/routes/users');
+const usersRoute 	  = require('./src/routes/users');
 const productsRoute   = require('./src/routes/products');
-const cartRoute 	    = require('./src/routes/cart');
+const cartRoute 	  = require('./src/routes/cart');
 
 
 app.listen(port);
--- src/routes/users.js
+++ src/routes/users.js
@@ -6,7 +6,7 @@ const users 		= require('../controllers/users');
 
 Route
 	.get('/', users.getUsers)
-	.post('/', users.createUsers)
+	// .post('/', users.createUsers)
 	.patch('/:id_user', users.updateUsers)
 
 	.patch('/password/:id_user', users.changePassword)
